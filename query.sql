1ce5222b1e17d3eff15d0ec9fd53573de358bcd7
Rafiul Islam <irafiul210@gmail.com>
2026-07-16 11:39:48 +0600

    wrote the second query matching sample output

--- query.sql
+++ query.sql
@@ -75,8 +75,14 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (505, 3, 102, 'C-20', 'Pending', 120.00);
 
 
-
+-- --------------------------------------------------------------------------------------------
 
 -- Query Start
--- 1.
-select match_id, fixture, base_ticket_price from matches where tournament_category = 'Champions League' and match_status = 'Available';
+-- 1.  Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+
+
+SELECT match_id, fixture, base_ticket_price FROM Matches WHERE tournament_category = 'Champions League' AND match_status = 'Available';
+
+
+-- 2. Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive)
+SELECT user_id, full_name, email FROM Users WHERE full_name LIKE 'Tanvir%' OR full_name LIKE '%Haque%';
